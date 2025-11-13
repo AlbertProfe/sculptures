0f47706da4049a853d40b92c0642f3a765324c7f
Albert Profe <albertprofe@gmail.com>
2025-11-13 09:26:29 +0000

    feat: contitinal rendering with switch case and useState

--- src/data.js
+++ src/data.js
@@ -5,6 +5,7 @@ export const sculptureList = [
     description:
       "Although Colvin is predominantly known for abstract themes that allude to pre-Hispanic symbols, this gigantic sculpture, an homage to neurosurgery, is one of her most recognizable public art pieces.",
     year: 2023,
+    location: "Oxford Street, 32, London",
     url: "https://i.imgur.com/Mx7dA2Y.jpg",
     alt: "A bronze statue of two crossed hands delicately holding a human brain in their fingertips.",
   },
